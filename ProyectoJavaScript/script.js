80aec22cbba0eb908a6c08a955a70b27a073dc47
mikel <UrlezagaMikel@gmail.com>
2024-06-07 03:43:04 +0200

    u

--- ProyectoJavaScript/script.js
+++ ProyectoJavaScript/script.js
@@ -19,3 +19,52 @@ function cerrarMenu() {
     text.style.display = 'none';
   });
 }
+
+
+document.getElementById('fetchButton').addEventListener('click', pillarDatos);
+document.getElementById('fetchButton').addEventListener('click', pillarDatos);
+document.getElementById('fetchButton').addEventListener('click', pillarDatos);
+document.getElementById('fetchButton').addEventListener('click', pillarDatos);
+document.getElementById('fetchButton').addEventListener('click', pillarDatos);
+document.getElementById('fetchButton').addEventListener('click', pillarDatos);
+document.getElementById('fetchButton').addEventListener('click', pillarDatos);
+document.getElementById('fetchButton').addEventListener('click', pillarDatos);
+
+function pillarDatos() {
+  fetch('https://randomuser.me/api/')
+      .then(response => response.json())
+      .then(data => {
+          const user = data.results[0];
+          const userDataDiv = document.getElementById('userFoto');
+          const userName = document.getElementById('userName');
+          const userEmail = document.getElementById('userEmail');
+          const userTelefono = document.getElementById('userTelefono');
+          const userUbi = document.getElementById('userUbi');
+          const userTexto = document.getElementById('userText');
+
+          userDataDiv.innerHTML = `
+            <div class="card">
+              <img src="${user.picture.large}" class="card-img-top" alt="Foto del usuario">
+            </div>
+          `;
+
+          userName.innerHTML = `
+            <h5 class="nombre">${user.name.first} ${user.name.last}</h5>
+          `;
+
+          userEmail.innerHTML = `
+            <strong>Email:</strong> ${user.email}
+          `;
+
+          userTelefono.innerHTML = `
+            <strong>Teléfono:</strong> ${user.phone}
+          `;
+
+          userUbi.innerHTML = `
+            <strong>Ubicación:</strong> ${user.location.city}, ${user.location.country}
+          `;
+      })
+      .catch(error => {
+          console.error('Error al obtener los datos:', error);
+      });
+}
