f829ee16b51ef972cde756c464038c786f480b36
mikel <UrlezagaMikel@gmail.com>
2024-06-20 22:13:08 +0200

    asdasd

--- foodbalancefront/src/views/ComidaFoodBalance.vue
+++ foodbalancefront/src/views/ComidaFoodBalance.vue
@@ -5,7 +5,13 @@ import HeaderFoodBalance from "../components/HeaderFoodBalance.vue";
 export default {
     data() {
         return {
-
+          people: [
+            { nombre: 'Mark', calorias: 'Otto', proteina: '@mdo', grasa: 'Otto', hc: '@mdo', fibra: '@mdo', extra: '@msddo',},
+            { nombre: 'Mark', calorias: 'Otto', proteina: '@mdo', grasa: 'Otto', hc: '@mdo', fibra: '@mdo', extra: '@mdo',},
+            { nombre: 'Mark', calorias: 'Otto', proteina: '@mdo', grasa: 'Otto', hc: '@mdo', fibra: '@mdo', extra: '@mdo',},
+            { nombre: 'Mark', calorias: 'Otto', proteina: '@mdo', grasa: 'Otto', hc: '@mdo', fibra: '@mdo', extra: '@mdo',},
+            { nombre: 'Mark', calorias: 'Otto', proteina: '@mdo', grasa: 'Otto', hc: '@mdo', fibra: '@mdo', extra: '@mdo',}
+          ]
         };
     },
     components: {
@@ -24,9 +30,11 @@ export default {
   <div id="medioComida">
     <div class="container text-center">
       <div class="row align-items-start">
+
+        <!-- Primera columna -->
         <div class="col">
 
-          <div class="dropdown">
+          <div class="dropdown" id="dropdownCategoriaComida">
             <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
               Selecciona Categoria
             </button>
@@ -37,67 +45,92 @@ export default {
             </ul>
           </div>
 
-          <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-              </tr>
-            </tbody>
-          </table>
+          <div class="table-responsive" id="tablaComida">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Nombre</th>
+                  <th scope="col">Calorias</th>
+                  <th scope="col">Proteina</th>
+                  <th scope="col">Grasa</th>
+                  <th scope="col">H.C.</th>
+                  <th scope="col">Fibra</th>
+                  <th scope="col">Extra</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(person, index) in people" :key="index">
+                  <th scope="row">{{ index + 1 }}</th>
+                  <td>{{ person.nombre }}</td>
+                  <td>{{ person.calorias }}</td>
+                  <td>{{ person.proteina }}</td>
+                  <td>{{ person.grasa }}</td>
+                  <td>{{ person.hc }}</td>
+                  <td>{{ person.fibra }}</td>
+                  <td>{{ person.extra }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
 
         </div>
+
+        <!-- Segunda columna -->
         <div class="col">
-          <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-              </tr>
-            </tbody>
-          </table>
+          <div style="display: flex;">
+            <p class="text-start" style="margin-top: 5.5%;">Nombre</p>
+            <div class="form-floating" style="margin-left: 3%;">
+              <textarea class="form-control" id="floatingTextarea" style="margin-top: 6.5%;"></textarea>
+            </div>
+
+            <p class="text-start" style="margin-left: 3%; margin-top: 5.5%;">Categoria</p>
+            <div class="form-floating" style="margin-left: 3%;">
+              <textarea class="form-control" id="floatingTextarea" style="margin-top: 6.5%;"></textarea>
+            </div>
+          </div>
+
+          <div class="container text-center">
+            <div class="row align-items-start">
+              <div class="col">
+                <p class="text-start" style="margin-top: 5%;">Calorias</p>
+          <div class="form-floating">
+            <textarea class="form-control" id="floatingTextarea"></textarea>
+          </div>
+
+          <p class="text-start">Proteina</p>
+          <div class="form-floating">
+            <textarea class="form-control" id="floatingTextarea"></textarea>
+          </div>
+
+          <p class="text-start">Grasa</p>
+          <div class="form-floating">
+            <textarea class="form-control" id="floatingTextarea"></textarea>
+          </div>
+
+          <p class="text-start">H.C.</p>
+          <div class="form-floating">
+            <textarea class="form-control" id="floatingTextarea"></textarea>
+          </div>
+
+          <p class="text-start">Fibra</p>
+          <div class="form-floating">
+            <textarea class="form-control" id="floatingTextarea"></textarea>
+          </div>
+
+          <p class="text-start">Extra</p>
+          <div class="form-floating">
+            <textarea class="form-control" id="floatingTextarea"></textarea>
+          </div>
+              </div>
+              <div class="col">
+                <button type="button" class="btn btn" id="botonAñadirComida">Añadir</button>
+              </div>
+            </div>
+          </div>
+
+
+
         </div>
       </div>
     </div>
